4bcbd295a68ecf998543aab1bc779cb5024f4ef7
Jeffrey Rios <jeffrey.rios@correounivalle.edu.co>
2019-09-26 10:17:29 -0500

    sesion 4

--- #04 Clases, v-bindClass/04.js
+++ #04 Clases, v-bindClass/04.js
@@ -1,33 +1,9 @@
 const app = new Vue({
     el: '#app',
     data: {
-        titulo: 'hola mundo con vue',
-        frutas: [
-            { nombre: 'pera', cantidad: 10 },
-            { nombre: 'manzana', cantidad: 0 },
-            { nombre: 'platano', cantidad: 50 }
-        ],
-        nuevaFruta: '',
-        total: 0
+
     },
     methods: {
-        agregarFruta() {
-            //cuando se quiera acceder a un elemento de data: usamos this
-            this.frutas.push({
-                nombre: this.nuevaFruta, cantidad: 0
-            });
-            this.nuevaFruta = '';
 
-        },
-
-    },
-    computed: {
-       sumarFrutas(){
-        this.total = 0;
-        for(fruta of this.frutas){
-            this.total = this.total + fruta.cantidad;
-        }
-        return this.total;
-       } 
     }
 });
